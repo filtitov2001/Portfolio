12ce2c4cebcfeb72ae7429d03f56d1b2535a9f47
Felix Titov <titow.feliks@yandex.ru>
2022-01-24 00:24:53 +0300

    created work and contact parts

--- src/components/body/contact/index.js
+++ src/components/body/contact/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Separator from '../../common/separator';
+import SocialContact from '../../common/social-contact';
+import './contact.css';
 
 function Contcact() {
   return (
-    <div>
-        This is contacts!
+    <div className='contact'>
+        <Separator />
+        <label className='section-title'>Conctact</label>
+        <div className='contact-container'>
+            <div className='contact-left'>
+                <p>Want to get in touch? Contact me on any of the platform</p>
+                <SocialContact />
+            </div>
+            <div className='left'>
+                <div className="about-photo">
+                        <img
+                            src={require("../../../assets/image/coding.png")}
+                            className="picture"
+                        />
+                </div>
+                <div className='download'>
+                    
+                    <a download href={require('../../../assets/test.pdf')}>
+                    <span class="lnr lnr-cloud-download download-icon"></span> Download Resume 
+                    </a>
+                </div>
+                </div>
+        </div>
     </div>
   );
 }
--- src/components/body/work/index.js
+++ src/components/body/work/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { WorkData } from '../../../data/work';
+import Separator from '../../common/separator';
+import WorkCard from './work-card';
 import './work.css';
 
 function Work() {
+    const data = WorkData;
   return (
     <div>
-      This is work!
+        <Separator />
+        <label className='section-title '>Work</label>
+        <div className='work-list'>
+            {data.map((item) => {
+                return (
+                     <WorkCard item={item} />
+                )
+            })}
+        </div>
     </div>
   );
 }
